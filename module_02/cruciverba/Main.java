2528432a77b82bd859a128637c85073940f2a9ec
MrSosu <49311976+MrSosu@users.noreply.github.com>
2023-02-02 12:33:05 +0100

    Add files via upload

---
+++ module_02/cruciverba/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[][] matrice = new char[][]{{'h', 'd', 'g', 'u', 'p', 'y', 's', 'p', 'a', 'o'}, {'t', 's', 'a', 'k', 'o', 'y', 'o', 'l', 'p', 's'}, {'r', 'c', 'm', 'c', 'n', 'r', 'i', 'a', 'a', 'b'}, {'i', 'o', 'b', 'a', 'i', 's', 'c', 't', 'l', 'a'}, {'g', 'r', 'e', 'l', 'f', 'a', 'c', 'e', 'o', 'l'}, {'l', 'f', 'r', 'a', 'l', 'l', 'u', 's', 'i', 'e'}, {'i', 'a', 'e', 'm', 'e', 'm', 'l', 's', 'l', 'n'}, {'a', 'n', 't', 'a', 'd', 'o', 'c', 'a', 'g', 'a'}, {'j', 'o', 't', 'r', 'b', 'n', 'd', 'g', 'o', 'd'}, {'l', 'y', 'o', 'o', 'u', 'e', 's', 'm', 's', 'm'}};
+
+        System.out.println(Cruciverba.cruciverba(matrice, "almto"));
+    }
+
+}
