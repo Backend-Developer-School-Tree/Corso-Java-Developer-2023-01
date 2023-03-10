117655cc766f191132c5fca43639a2559e998a25
Davide <davidefella@users.noreply.github.com>
2023-03-10 12:40:08 +0100

    get all users

--- module_13/Live Coding/demo_rest_api/src/main/java/org/example/App.java
+++ module_13/Live Coding/demo_rest_api/src/main/java/org/example/App.java
@@ -3,13 +3,24 @@ package org.example;
 import com.google.gson.Gson;
 import org.example.model.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
-public class App
-{
+public class App {
+
+    private static Map<Integer, User> users = new HashMap<>(); //Simulo un DB con una mappa
+
     public static void main( String[] args ) {
 
-        System.out.println( "Hello World!" );
+        //Mi creo dei dati di test all'avvio del programma
+        User u1 = new User( 1, "username1", "myEmail1@gmail.com");
+        User u2 = new User( 2, "username2", "myEmail2@gmail.com");
+        User u3 = new User( 3, "username3", "myEmail3@gmail.com");
+        users.put(1, u1);
+        users.put(2, u2);
+        users.put(3, u3);
 
         // Porta su cui deve girare il processo, è opzionale nel caso prende la default
         port(8080);
@@ -25,11 +36,32 @@ public class App
         });
 
         /*
-        * Servizio web che torna un utente dummy
+        * Servizio web che torna un  utente dummy
         * */
         get("/user", (req, res)->{
-            User u1 = new User(1, "username1", "myEmail@gmail.com");
-            return new Gson().toJson(u1);
+            //mi aspetto nel sevizio una proprietà "id" dopo il ?
+            int userId = Integer.valueOf(req.queryParams("id"));
+
+            //Il secondo avrà &
+            String username = req.queryParams("username");
+
+            // Cerco nel Database utente con id e/o username che prendo da params
+            User uDummy = new User( userId, username, "myEmail@gmail.com");
+
+            //Simulo che non ho trovato nessun utente nel database con un parametro 0
+            if(userId==0){
+                res.status(404);
+                return new Gson().toJson("User not found");
+            }
+
+            res.type("application/json"); //il tipo di ritorno è json
+            return new Gson().toJson(uDummy);
+        });
+
+        //Get all users
+        get("/users", (req, res)->{
+            res.type("application/json");
+            return new Gson().toJsonTree(users.values());
         });
     }
 }
