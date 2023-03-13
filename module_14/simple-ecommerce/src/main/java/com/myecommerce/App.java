cbbcb21c147bd9cb0e38a2bb5e3708a5dc919017
Davide <davidefella@users.noreply.github.com>
2023-03-13 14:59:19 +0100

    add post and get all products

--- module_14/simple-ecommerce/src/main/java/com/myecommerce/App.java
+++ module_14/simple-ecommerce/src/main/java/com/myecommerce/App.java
@@ -1,13 +1,54 @@
 package com.myecommerce;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+import com.myecommerce.model.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static spark.Spark.*;
+
+public class App {
+
+    private static Set<Product> products = new HashSet<>();
+
+    private static Gson gson = new Gson();
+
+    public static void main(String[] args) {
+
+        /*DATI DI TEST PRE-CARICATI*/
+        Product p1 = new Product(1L, 100, 9.99, "P1", "Product P1");
+        products.add(p1);
+
+        port(8080); //opzionale, indica su quale porta è in ascolto il mio processo 
+
+        path("/ecommerce", () -> {
+            path("/product", () -> {
+                        post("/add", (req, res) -> {
+                            Product newProduct = gson.fromJson(req.body(), Product.class);
+
+                            /* Dall'esterno mi arriva un prodotto già presente -> Errore */
+                            if (products.contains(newProduct)) {
+                                res.status(400);
+                                return "product already present";
+                            }
+
+                            products.add(newProduct);
+                            res.status(201);
+                            return "ok";
+                        });
+                    }
+            );
+
+            path("/products", () -> {
+                get("/available", (req, res) -> {
+                            res.type("application/json");
+                            res.status(200);
+
+                            return gson.toJson(products);
+                        }
+                );
+            });
+        });
     }
 }
