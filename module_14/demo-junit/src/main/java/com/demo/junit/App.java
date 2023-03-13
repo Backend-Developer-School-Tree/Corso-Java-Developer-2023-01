e293c861fe2ca8faeaf5335aaed58d2112b3dfb3
Davide <davidefella@users.noreply.github.com>
2023-03-13 09:37:15 +0100

    module 14 init

---
+++ module_14/demo-junit/src/main/java/com/demo/junit/App.java
@@ -0,0 +1,13 @@
+package com.demo.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
