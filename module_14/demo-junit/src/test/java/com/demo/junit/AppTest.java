7b97ae9795362c68afb94f98f82a368b77eb9403
Davide <davidefella@users.noreply.github.com>
2023-03-13 10:12:59 +0100

    add tests

--- module_14/demo-junit/src/test/java/com/demo/junit/AppTest.java
+++ module_14/demo-junit/src/test/java/com/demo/junit/AppTest.java
@@ -1,20 +1,52 @@
 package com.demo.junit;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    List<String> myStringList;
+
+    @BeforeAll
+    static void beforeAllMethods(){
+        System.out.println("Before all methods");
+    }
+
+    @BeforeEach
+    void setUpBeforeEach(){
+        System.out.println("Hello World");
+
+        myStringList = new ArrayList<>();
+
+        myStringList.add("My value 1");
+        myStringList.add("My value 2");
+        myStringList.add("My value 3");
+
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void notNullListTest(){
+        assertNotNull(myStringList);
+    }
+
+    @Test
+    void sizeListTest(){
+        assertEquals(myStringList.size(), 3);
+    }
+
+    @Test
+    void addElement2ListTest(){
+        myStringList.add("Value 4");
+
+        assertTrue(myStringList.size() == 4);
     }
 }
