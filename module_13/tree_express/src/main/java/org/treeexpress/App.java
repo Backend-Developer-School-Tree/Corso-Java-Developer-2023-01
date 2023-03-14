1f8a7bae45098b0bc3405348e16276493618d967
Davide <davidefella@users.noreply.github.com>
2023-03-14 09:50:11 +0100

    WIP - add user

--- module_13/tree_express/src/main/java/org/treeexpress/App.java
+++ module_13/tree_express/src/main/java/org/treeexpress/App.java
@@ -1,8 +1,34 @@
 package org.treeexpress;
 
-public class App 
-{
+import com.google.gson.Gson;
+import org.treeexpress.model.Delivery;
+import org.treeexpress.model.TreeExpressUser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+import static spark.Spark.*;
+
+public class App {
+
+    private static Gson gson = new Gson();
+
+    Map<Long, TreeExpressUser> treeExpressUser = new HashMap<>();
+    List<Delivery> deliveries = new ArrayList<>();
+
     public static void main( String[] args ) {
+
+        path("/express", () -> {
+            path("/treeexpressuser", () ->{
+                post("add", (req, res) -> {
+
+                    return "ok";
+                });
+            });
+        });
 
     }
 }
