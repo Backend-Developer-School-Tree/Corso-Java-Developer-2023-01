85a9ebb86d0b326698bb05970bdc8e4d087b1835
Davide <davidefella@users.noreply.github.com>
2023-03-14 10:03:59 +0100

    Add user; Get all users

--- module_13/tree_express/src/main/java/org/treeexpress/App.java
+++ module_13/tree_express/src/main/java/org/treeexpress/App.java
@@ -16,16 +16,29 @@ public class App {
 
     private static Gson gson = new Gson();
 
-    Map<Long, TreeExpressUser> treeExpressUser = new HashMap<>();
+    static Map<Long, TreeExpressUser> treeExpressUsers = new HashMap<>();
     List<Delivery> deliveries = new ArrayList<>();
 
     public static void main( String[] args ) {
+        port(8081); //8081 perché 8080 era già occupata :)
 
         path("/express", () -> {
             path("/treeexpressuser", () ->{
-                post("add", (req, res) -> {
+                post("/add", (req, res) -> {
 
+                    //TODO: implementare controllo su ID
+                    TreeExpressUser newExpressUser = gson.fromJson(req.body(),TreeExpressUser.class);
+
+                    treeExpressUsers.put(newExpressUser.getId(), newExpressUser);
+
+                    res.status(201); //201 --> creato
                     return "ok";
+                });
+
+                get("/all", (req, res) -> {
+
+                    res.status(201); //201 --> creato
+                    return gson.toJson(treeExpressUsers);
                 });
             });
         });
